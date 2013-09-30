cea400e227dd93e623950801cf778afcbcac0ebd
Ryan <ryan.cooke@gmail.com>
2013-09-29 21:20:10 -0400

    add memberships to groups

--- lib/ruby-box/group.rb
+++ lib/ruby-box/group.rb
@@ -3,6 +3,20 @@ module RubyBox
 
     has_many :memberships
 
+    def add_member member, role="member"
+      uri = URI.parse( "#{RubyBox::API_URL}/group_memberships" )
+      request = Net::HTTP::Post.new( uri.request_uri )
+      request.body = JSON.dump({
+        "user" => {"id" => member.id},
+        "group" => {"id" => id},
+        "role" => role
+      })
+
+      @session.request(uri, request)
+
+      self
+    end
+
     private
 
     def resource_name
